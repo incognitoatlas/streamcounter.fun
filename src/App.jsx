f6616de982d9374bc3efdd61acd960cd4e0ac097
Michael Cortese <90201909+incognitoatlas@users.noreply.github.com>
2021-10-22 01:53:38 +0000

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ function App() {
   const [count, incrementCount] = useState(0);
 
   return (
-    <button className="button1 noselect" onClick={() => incrementCount(count+1)}>Count: {count}</button>
+    <div>
+      <div id="buttonClass" className="centered">
+      <h1>You have</h1>
+      <input id="nameInput" placeholder="Click me to edit!" autoFocus></input>
+      <h1>{count} times!</h1>
+      </div>
+      <button className="button1 noselect" onClick={() => incrementCount(count+1)}>Add Count</button>
+    </div>
   );
 }
 
