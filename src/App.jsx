bd75ef631e046f4905f9598d24552ec36a35095b
Michael <90201909+incognitoatlas@users.noreply.github.com>
2021-10-26 17:35:35 +0000

    removed HandleSpace

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import './App.css';
 
 function App() {
   const [count, incrementCount] = useState(0);
-  const handleSpace = (e) => {
-    if (e.keyCode === 32) {
-      incrementCount(count + 1);
-    }
-  };
 
   return (
     <div>
@@ -21,13 +16,13 @@ function App() {
         <button className="button1 noselect inrow" onClick={() => incrementCount(count - 1)}>-</button>
       </div>
       <div className="btnrow">
-        <button className="button2 noselect ten" onClick={() => incrementCount(count + 10)} onKeyDown={handleSpace}>+10</button>
-        <button className="button2 noselect ten" onClick={() => incrementCount(count - 10)} onKeyDown={handleSpace}>-10</button>
-        <button className="button2 noselect ten" onClick={() => incrementCount(count + 100)} onKeyDown={handleSpace}>+100</button>
-        <button className="button2 noselect ten" onClick={() => incrementCount(count - 100)} onKeyDown={handleSpace}>-100</button>
+        <button className="button2 noselect ten" onClick={() => incrementCount(count + 10)}>+10</button>
+        <button className="button2 noselect ten" onClick={() => incrementCount(count - 10)}>-10</button>
+        <button className="button2 noselect ten" onClick={() => incrementCount(count + 100)}>+100</button>
+        <button className="button2 noselect ten" onClick={() => incrementCount(count - 100)}>-100</button>
       </div>
       <div id="center">
-      <button className="button2 noselect" id="resetbtn" onClick={() => incrementCount(0)} onKeyDown={handleSpace}>Reset Counter</button>
+      <button className="button2 noselect" id="resetbtn" onClick={() => incrementCount(0)}>Reset Counter</button>
       </div>
     </div>
   );
